fab0517f6af4370a8597d7690fe4251862e779b6
Akash sahu <sahuakash098@gmail.com>
2018-09-30 16:49:38 +0530

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ticktee::Application.config.secret_key_base = '814b912804c87ffb943bd4bb90284eedca674dc82b3b240e92a36c3313853b61e0285bc73e37571b34933d1b3750f8ef948bbf1307eeb3aaf58767def3e5f343'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ticktee::Application.config.session_store :cookie_store, key: '_ticktee_session'
