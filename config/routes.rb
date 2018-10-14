743d17f24381fa581b1cb1f0c3d330175f2c61e9
Akash sahu <sahuakash098@gmail.com>
2018-10-14 16:42:47 +0530

    Add the ability to create users through the admin backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ticktee::Application.routes.draw do
+  get "users/index"
   get "application/index"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
@@ -22,6 +23,7 @@ Ticktee::Application.routes.draw do
       root "application#index"
 
       resources :projects, only: [:new, :create, :destroy]
+      resources :users
      end
 
 
